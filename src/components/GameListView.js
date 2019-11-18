6b923ee5021e7b1e9ba42184fc24b3e9cbe18b7a
capntechbeard <capntechbeard@gmail.com>
2019-11-18 15:30:58 -0600

    Font Awesome react install

--- src/components/GameListView.js
+++ src/components/GameListView.js
@@ -3,13 +3,26 @@ import React from "react";
 export const GameListView = props => {
   const name = props.name;
   const id = props.id;
-  const description = props.description;
   const metacritic = props.metacritic;
   const rating = props.rating;
   const released = props.released;
   const icon = props.icon;
-  const website = props.website;
-  // const parent_platforms = props.parent_platforms;
+  const platforms = props.parent_platforms;
+
+  // const TEST = ({ platforms }) => (
+  //   <>
+  //     {platforms.map(platform => (
+  //       <div className="platform" key={platform.call}>
+  //         {platform.call}
+  //       </div>
+  //     ))}
+  //   </>
+  // );
+
+  console.log("<====platforms====>");
+  console.log(TEST);
+  console.log("<====platforms====>");
+
   const outerWrapperStyle = {
     backgroundImage: "url(" + icon + ")"
   };
@@ -29,7 +42,8 @@ export const GameListView = props => {
         <h3>{metacritic}</h3>
         {rating > 0 && <h3>{rating}</h3>}
         {id}
-
+        <br />
+        {/* {TEST} */}
         <p>Released: {released}</p>
       </div>
     </div>
