cf82996c311fb1384e3e31dbbfe5f3302e98d816
capntechbeard <capntechbeard@gmail.com>
2019-12-05 18:45:21 -0600

    Updates

---
+++ precache-manifest.1f6a1faef7ec3c7b419d5dcf3b1a0b56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22389df57c03dc1fc8ad79f516b118c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b38bfbca0c3558c064cb",
+    "url": "/static/css/main.4d3961bf.chunk.css"
+  },
+  {
+    "revision": "20f42e7c034cc06578df",
+    "url": "/static/js/2.2edee09e.chunk.js"
+  },
+  {
+    "revision": "b38bfbca0c3558c064cb",
+    "url": "/static/js/main.5ca3b9a0.chunk.js"
+  },
+  {
+    "revision": "6a69b382419ae6847553",
+    "url": "/static/js/runtime-main.39d4461a.js"
+  }
+]);
