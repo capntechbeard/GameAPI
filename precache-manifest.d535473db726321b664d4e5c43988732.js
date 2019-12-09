f78e4dd0fb5f02c0e1da00bec647fc3568c59371
capntechbeard <capntechbeard@gmail.com>
2019-12-09 17:50:43 -0600

    Updates

---
+++ precache-manifest.d535473db726321b664d4e5c43988732.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a31e55f535477d0b9f5ab1798ab64a",
+    "url": "/GameAPI/index.html"
+  },
+  {
+    "revision": "2b32b81260723397b61c",
+    "url": "/GameAPI/static/css/main.c5d79b90.chunk.css"
+  },
+  {
+    "revision": "04b3e6d2be69772ab6b7",
+    "url": "/GameAPI/static/js/2.c5a109ec.chunk.js"
+  },
+  {
+    "revision": "2b32b81260723397b61c",
+    "url": "/GameAPI/static/js/main.7644b43b.chunk.js"
+  },
+  {
+    "revision": "5988118d1df75f8bb6a7",
+    "url": "/GameAPI/static/js/runtime-main.e3f43853.js"
+  },
+  {
+    "revision": "22f4b562883f3bdef3f5c776c15e961c",
+    "url": "/GameAPI/static/media/EpicGames.22f4b562.svg"
+  },
+  {
+    "revision": "1be9a0deb49ac9e7fd4e93d0f3430e42",
+    "url": "/GameAPI/static/media/GOG.1be9a0de.svg"
+  },
+  {
+    "revision": "71bbcb5df0a5d616c7286ac3f22f73f1",
+    "url": "/GameAPI/static/media/NintendoSwitch.71bbcb5d.svg"
+  }
+]);
